289355bdd87d020cf4c4f6f0c3a45d64ea761d6f
kentjorel15-pixel <kentjorel15@gmail.com>
2026-09-17 23:43:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function firstMessage() {
-    alert("That's why I didn't kiss you hmp... Veryyy madd !!! 😤💗");
-
     document.getElementById("also").style.display = "none";
     document.getElementById("mad").style.display = "none";
+
+    document.getElementById("message").innerHTML =
+        "That's why I didn't kiss you hmp... Veryyy madd !!! 😤💗";
 }
 
 function secondMessage() {
-    alert("So, don't match my energy and lambing me 😤🌸💕");
+    document.getElementById("message").innerHTML =
+        "So, don't match my energy and lambing me 😤🌸💕";
 }
