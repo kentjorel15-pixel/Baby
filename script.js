427ba248d8669b8d953b455340049e718c2e19f2
kentjorel15-pixel <kentjorel15@gmail.com>
2026-09-18 00:19:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 function showLablab() {
+    document.getElementById("hello").style.display = "none";
     document.getElementById("lablab").style.display = "block";
 }
 
 function showAngry() {
-    document.getElementById("also").style.display = "none";
-    document.getElementById("mad").style.display = "none";
-
+    document.getElementById("lablab").style.display = "none";
     document.getElementById("angry").style.display = "block";
 }
 
 function showLove() {
     document.getElementById("angry").style.display = "none";
-
     document.getElementById("love").style.display = "block";
 }
