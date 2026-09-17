4e0417fadbcd22f073b6fdfa96300f031ed7fdf7
kentjorel15-pixel <kentjorel15@gmail.com>
2026-09-18 00:24:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 function showLablab() {
+
     document.getElementById("hello").style.display = "none";
+
     document.getElementById("lablab").style.display = "block";
+
 }
+
 
 function showAngry() {
+
     document.getElementById("lablab").style.display = "none";
+
     document.getElementById("angry").style.display = "block";
+
 }
 
+
 function showLove() {
+
     document.getElementById("angry").style.display = "none";
+
     document.getElementById("love").style.display = "block";
+
 }
