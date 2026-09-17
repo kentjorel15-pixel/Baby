805de01154a825a417a8ed8b648430036f27f328
kentjorel15-pixel <kentjorel15@gmail.com>
2026-09-17 23:31:14 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function firstMessage() {
+    document.getElementById("message").innerHTML =
+        "That's why I didn't kiss you hmp... Veryyy madd !!!";
+}
+
+function secondMessage() {
+    document.getElementById("message").innerHTML =
+        "So, don't match my energy and lambing me 😤";
+}
